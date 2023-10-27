000f55b6004954ccd1b1dc0a45d49537e88b1a73
shalaka261993 <shalakachoudhary01@gmail.com>
2023-10-27 23:57:00 +0530

    this is my second project commit

---
+++ shreesha.py
@@ -0,0 +1,71 @@
+a=5
+print("type of a: ", type(a))
+
+a="5"
+print("type of a: ", type(a))
+
+b=5.0
+print("type of b: ", type(b))
+
+c=2+4j
+print("type of c: ", print(c))
+
+c=2 + 4j
+print(type(c))
+
+a=True
+print(type(a))
+
+b= False
+print("type of b: ", type(b))
+
+a="python"
+
+print(len(a))
+
+b=a.find("y")
+print(b)
+
+b=a.find("n")
+print(b)
+
+a="python"
+b='test'.join(a)
+print(b)
+a="       python"
+b=a.strip()
+print(b)
+
+a='python class is there or not'
+b=a.split()
+print(b)
+
+# split in string into list
+
+#print(a.zfill(10))
+#a="hello"
+#print(a.zfill(10)
+
+a='python'
+print(a.zfill(10))
+
+a="hello my world"
+print(a.count('l'))
+
+a='PYTHON'
+b=a.lower()
+print(b)
+
+a='python'
+b=a.upper()
+print(b)
+
+b=a.capitalize()
+print(b)
+
+
+b=a.center(10)
+print(b)
+
+b=a.replace("p","c")
+print(b)
